b06cf3cffdc5d1af9d160f914537b8a10c8945bb
Hérikz Nawarro <herikz.nawarro@gmail.com>
2018-03-27 07:41:25 -0300

    Add files via upload

---
+++ src/http/routes.js
@@ -0,0 +1,47 @@
+const routes = server => {
+  server.route({
+    method:'GET',
+    path:'/axios',
+    handler: (request, h) => h.file('axios.min.js')
+  })
+
+  server.route({
+    method:'GET',
+    path:'/getSubmit',
+    handler: (request, h) => {
+      const {
+        user,
+        password
+      } = request.query
+
+      return user && password
+        ? `Olá ${user}, a sua senha é ${password}`
+        : JSON.stringify(request.query, null, 4)
+    }
+  })
+
+  server.route({
+    method:'GET',
+    path:'/hello',
+    handler: (request, h) => h.file('giphy.webp')
+  })
+
+  server.route({
+    method: ['OPTIONS', 'POST'],
+    path:'/postSubmit',
+    handler: (request, h) => {
+      const {
+        user,
+        password
+      } = request.query
+
+      console.log(user, ':', password)
+
+      return user && password
+        ? `Olá ${user}, a sua senha é ${password}`
+        : JSON.stringify(request.query, null, 4)
+    }
+  })
+}
+
+module.exports = routes
---
+++ src/server/index.js
@@ -0,0 +1,40 @@
+import Hapi from 'hapi'
+import { plugin as inert } from 'inert'
+import path from 'path'
+import routes from '../http/routes'
+
+// Create a server with a host and port
+const server = Hapi.server({
+  host: 'localhost',
+  port: 8000,
+  routes: {
+    cors: true,
+    // cors: {
+    //   credentials: true,
+    //   headers: ['Accept', 'Authorization', 'Content-Type', 'If-None-Match', 'Access-Control-Allow-Origin'],
+    //   origin: ['*']
+    // },
+    files: {
+      relativeTo: path.join(__dirname, '../../static')
+    }
+  }
+})
+
+routes(server)
+
+// Start the server
+const start = async () => {
+  try {
+    await server.register(inert)
+    await server.start()
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  console.log(`Server running at: ${server.info.uri}`)
+}
+
+module.exports = {
+  start
+}
